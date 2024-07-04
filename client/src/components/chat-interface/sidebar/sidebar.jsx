fee9f6cc2d8e40f18059602e4eb80d9e8a000f26
wheatstore <yoonate25@gmail.com>
2024-07-04 09:34:59 -0700

    added a upwards dropdown menu that when toggled displayed the user's settings

--- client/src/components/chat-interface/sidebar/sidebar.jsx
+++ client/src/components/chat-interface/sidebar/sidebar.jsx
@@ -9,19 +9,20 @@ import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 function Sidebar() {
   const [user] = useAuthState(auth);
   const [previousChats, setPreviousChats] = useState([]);
+  const [showSettings, setShowSettings] = useState(false);
 
   const navigate = useNavigate();
 
-  // function to get all past chats
+  // Function to get all past chats
   useEffect(() => {
     if (user) {
-      const previousChatsRef = collection(db, 'users', user.uid, 'previousChats');
-      const q = query(previousChatsRef, orderBy('timestamp', 'asc'));
+      const previousChatsRef = collection(db, "users", user.uid, "previousChats");
+      const q = query(previousChatsRef, orderBy("timestamp", "asc"));
 
       const unsubscribe = onSnapshot(q, (snapshot) => {
-        const previousChatsList = snapshot.docs.map(doc => ({
+        const previousChatsList = snapshot.docs.map((doc) => ({
           id: doc.id,
-          ...doc.data()
+          ...doc.data(),
         }));
         setPreviousChats(previousChatsList);
       });
@@ -49,29 +50,38 @@ function Sidebar() {
             focusable="false"
             tabIndex="-1"
           >
-            <path
-              d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12m12.524-3.753a1 1 0 0 1 1.228 1.228l-1.12 4.105a1.5 1.5 0 0 1-1.052 1.052l-4.105 1.12a1 1 0 0 1-1.228-1.228l1.12-4.105a1.5 1.5 0 0 1 1.052-1.052z"
-              fill="currentColor"
-              fillRule="evenodd"
-            />
-          </svg> Explore
+            <path d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12m12.524-3.753a1 1 0 0 1 1.228 1.228l-1.12 4.105a1.5 1.5 0 0 1-1.052 1.052l-4.105 1.12a1 1 0 0 1-1.228-1.228l1.12-4.105a1.5 1.5 0 0 1 1.052-1.052z" 
+            fill="currentColor" 
+            fillRule="evenodd">
+            </path>
+          </svg>
+          Explore
         </button>
       </div>
-      <div className="user-account-information-container">
-      </div>
+      <div className="user-account-information-container"></div>
       <div className="previous-chats-container">
         <h3 className="previous-chats-heading">Chats</h3>
-        {previousChats.map(chat => (
+        {previousChats.map((chat) => (
           <div onClick={() => navigate(`/chat/${chat.character}`)} key={chat.id} className="chat-item">
             <img className="chat-item-image" src={chat.img} alt="" />
             <p>{chat.name}</p>
           </div>
         ))}
       </div>
-      <div className="user-sidebar-account-information">
+      <div onClick={() => setShowSettings((prev) => !prev)} className="user-sidebar-account-information">
         <img src={user.photoURL} alt="" />
         <h3>{user.displayName}</h3>
+        <button className="dropdown-toggle" >
+          <span>▼</span>
+        </button>
       </div>
+      {showSettings && (
+          <ul className="sidebar-dropdown-menu">
+            <li className="sidebar-dropdown-item">Public profile</li>
+            <li className="sidebar-dropdown-item">Settings</li>
+            <li className="sidebar-dropdown-item">Logout</li>
+          </ul>
+        )}
     </div>
   );
 }
