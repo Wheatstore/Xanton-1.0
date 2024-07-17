176b65c21cb6c22ae9aee5e3001284765360637f
wheatstore <yoonate25@gmail.com>
2024-07-17 11:24:48 -0700

    added a new api endpoint

--- client/src/components/feedback/feedback.jsx
+++ client/src/components/feedback/feedback.jsx
@@ -1,10 +1,33 @@
 import { useState } from "react"
 import "./feedback.css"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "../../firebase"
 
 function Feedback(){
+    const [user] = useAuthState(auth)
+    const navigate = useNavigate()
     const [inputFeedback, setInput] = useState("")
     const [email, setEmail] = useState("")
     const [feedbackDescription, setDescription] = useState("")
+
+    const sendFeedback = async () => {
+        try {
+            const body = {
+                sender: user,
+                title: inputFeedback,
+                email: email,
+                description: description
+            }
+            const response = await axios.post("https://xanton-1-0-server.vercel.app/api/feedback", body)
+            if (response.status === 200){
+                navigate("/user")
+            }
+        } catch (error){
+            console.error("There was an error submiting feedback", error)
+        }
+    }
 
     return (
         <>
@@ -17,7 +40,7 @@ function Feedback(){
                     <input placeholder="Title the problem" type="text" onChange={(e) => setInput(e.target.value)} value={inputFeedback} required/>
                     <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder="What is an email so that I can contact you further if needed"/>
                     <textarea onChange={(e) => setDescription(e.target.value)} value={feedbackDescription} placeholder="Describe the problem in detail..." required></textarea>
-                    <button>Submit</button>
+                    <button onClick={sendFeedback}>Submit</button>
                 </div>
                 <div className="contact-support-div">
                     <h3><span class="material-symbols-outlined">mail</span> support@xantonai.com</h3>
--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-const { updateMessage, setNewBot, createNewUser } = require("./admin");
+const nodeMailer = require("nodemailer")
+const { updateMessage, setNewBot, createNewUser, addFeedback } = require("./admin");
 
 const API_KEY = process.env.OPENAI_API_KEY;
 
@@ -15,7 +16,7 @@ const app = express();
 
 // Enable CORS for specific origin
 const corsOptions = {
-    origin: 'https://xanton-1-frontend.vercel.app',
+    origin: 'https://www.xantonai.com',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
@@ -64,6 +65,16 @@ app.post("/api/create-new-bot", async (req, res) => {
         res.status(500).json({ error: 'Failed to create new bot' });
     }
 });
+
+app.post("/api/feedback", async (req, res)=> {
+    const {sender, title, email, description} = req.body
+    try{
+        await addFeedback(sender, title, email, description)
+        res.status(200)
+    } catch (error){
+        console.error("There was an error sending an email", error)
+    }
+})
 
 app.post("/api/create/user", async (req, res) => {
     try {
