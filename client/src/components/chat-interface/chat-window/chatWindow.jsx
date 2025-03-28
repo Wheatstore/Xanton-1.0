17a6bb1e858e53c69fd100b164562b50b6e2950e
Wheatstore <yoonate25@gmail.com>
2025-03-27 20:20:06 -0700

    Installed some new dependencies and modified the root server.

--- client/src/components/chat-interface/chat-window/chatWindow.jsx
+++ client/src/components/chat-interface/chat-window/chatWindow.jsx
@@ -160,7 +160,7 @@ function ChatWindow() {
                 
                 // Send message to backend API
                 const response = await axios.post(
-                    `http://localhost:3000/api/chat/${user.uid}/${params.id}/${params.name}`, 
+                    `https://historia-backend-8bvz.onrender.com//${user.uid}/${params.id}/${params.name}`, 
                     { message: messageText, messageId: docRef.id }
                 );
                 
